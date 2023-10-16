86efc96972e392a0aaa94a45e34efe7cfd7fa107
mavix21 <marcevizcarra2109@hotmail.com>
2023-10-15 22:59:52 -0500

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import './style.scss';
+import './components/layout/Cover/Cover.js';
+import './components/layout/Center/Center.js';
+import './components/layout/Stack/Stack.js';
+import './components/layout/Box/Box.js';
+import './components/layout/Cluster/Cluster.js';
